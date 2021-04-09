d0d7dce885e1fc130cdd48a61041edeec3ebfa32
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2021-04-09 17:19:08 -0400

    Build it

--- src/index.js
+++ src/index.js
@@ -232,6 +232,6 @@ export let NodeEditor = (
 NodeEditor = React.forwardRef(NodeEditor);
 export { FlumeConfig, Controls, Colors } from "./typeBuilders";
 export { RootEngine } from "./RootEngine";
-export { Control } from "./components/Control/Control";
+export Control from "./components/Control/Control";
 export const useRootEngine = (nodes, engine, context) =>
   Object.keys(nodes).length ? engine.resolveRootNode(nodes, { context }) : {};
